8d8712ef33cb81f3d53cfcf3bc72262732523854
Aleksandr <aldanchenko@gmail.com>
2020-11-29 20:01:00 +0200

    feat: added basic authentication for cli.

--- snyk-visual-studio-plugin/SnykGeneralSettingsUserControl.cs
+++ snyk-visual-studio-plugin/SnykGeneralSettingsUserControl.cs
@@ -7,9 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Net;
 
 namespace snyk_visual_studio_plugin
 {
+    [DataContract]
+    internal class SnykVerifyRequest
+    {
+        [DataMember]
+        internal string token;
+    }
+
+    [DataContract]
+    internal class SnykVerifyResponse
+    {
+        [DataMember(Name = "ok")]
+        internal bool isSuccessful;
+
+        [DataMember(Name = "api")]
+        internal string token;
+    }
+
     public partial class SnykGeneralSettingsUserControl : UserControl
     {
         internal SnykGeneralOptionsDialogPage optionsDialogPage;
@@ -29,7 +50,51 @@ namespace snyk_visual_studio_plugin
         
         private void authenticateButton_Click(object sender, EventArgs e)
         {
+            using (var webClient = new WebClient())
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
+                webClient.Headers.Add("user-agent", "VisualStudioSnykExtension");
+
+                string endpointUri = "http://snyk.io";
+                Guid newToken = Guid.NewGuid();
+
+                string token = newToken.ToString();
+
+                string loginUri = endpointUri + "/login?token=" + token + "&from=VisualStudioSnykExtension";
+
+                System.Diagnostics.Process.Start(loginUri);
+
+                var httpWebRequest = (HttpWebRequest) WebRequest.Create("https://snyk.io/api/v1/verify/callback");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = GetSnykVerifyRequestJson(token);
+
+                    streamWriter.Write(json);
+                }
+
+                var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
+                string jsonResponseStr = null;
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    jsonResponseStr = streamReader.ReadToEnd();
+                }
+                
+                SnykVerifyResponse verifyResponse = GetSnykVerifyResponse(jsonResponseStr);
+
+                if (verifyResponse.isSuccessful)
+                {
+                    tokenTextBox.Text = verifyResponse.token;
+                } else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
         }
 
         private void tokenTextBox_TextChanged(object sender, EventArgs e)
@@ -50,6 +115,34 @@ namespace snyk_visual_studio_plugin
         private void ignoreUnknownCACheckBox_CheckedChanged(object sender, EventArgs e)
         {
             optionsDialogPage.IgnoreUnknownCA = ignoreUnknownCACheckBox.Checked;
+        }
+
+        private string GetSnykVerifyRequestJson(string token)
+        {
+            SnykVerifyRequest snykVerifyRequest = new SnykVerifyRequest();
+            snykVerifyRequest.token = token;
+
+            var memoryStream = new MemoryStream();
+            var jsonSerializer = new DataContractJsonSerializer(typeof(SnykVerifyRequest));
+            jsonSerializer.WriteObject(memoryStream, snykVerifyRequest);
+            memoryStream.Position = 0;
+
+            var streamReader = new StreamReader(memoryStream);
+            
+            return streamReader.ReadToEnd();
+        }
+
+        private SnykVerifyResponse GetSnykVerifyResponse(string json)
+        {
+            var verifyResponse = new SnykVerifyResponse();
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            var jsonSerializer = new DataContractJsonSerializer(verifyResponse.GetType());
+
+            verifyResponse = jsonSerializer.ReadObject(memoryStream) as SnykVerifyResponse;
+
+            memoryStream.Close();
+
+            return verifyResponse;
         }
     }  
 }
