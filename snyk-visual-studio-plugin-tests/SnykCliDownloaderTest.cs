bd79c673ea7c25e673cf1a7d4b6beabfd6ad2c64
Aleksandr <aldanchenko@gmail.com>
2020-12-01 12:35:04 +0200

    feat: added unit tests project.

---
+++ snyk-visual-studio-plugin-tests/SnykCliDownloaderTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace snyk_visual_studio_plugin_tests
+{
+    [TestClass]
+    public class SnykCliDownloaderTest
+    {
+        [TestMethod]
+        public void GetLatestReleaseInfo()
+        {
+        }
+    }
+}
