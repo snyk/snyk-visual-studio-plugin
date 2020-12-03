7bb3f7c4b57274cd0b75a4ad2059031b3b98cdbc
Aleksandr <aldanchenko@gmail.com>
2020-12-03 18:50:12 +0200

    feat: added Json deserializer class.

---
+++ snyk-visual-studio-plugin/Json.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Snyk.VisualStudio.Extension.Util
+{
+    class Json
+    {
+        public static object Deserialize(string sourceJson, Type resultType)
+        {
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(sourceJson));
+            var jsonSerializer = new DataContractJsonSerializer(resultType);
+
+            var result = jsonSerializer.ReadObject(memoryStream);
+
+            memoryStream.Close();
+
+            return result;
+        }
+    }
+}
