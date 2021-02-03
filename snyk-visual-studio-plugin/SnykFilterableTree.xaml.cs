109d1230d0b6eba3aa4c84886e5d42ecc5375b1e
Aleksandr <aldanchenko@gmail.com>
2021-02-03 10:42:28 +0200

    fix: dark theme select issue.

--- snyk-visual-studio-plugin/SnykFilterableTree.xaml.cs
+++ snyk-visual-studio-plugin/SnykFilterableTree.xaml.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Snyk.VisualStudio.Extension.UI
@@ -23,7 +24,7 @@ namespace Snyk.VisualStudio.Extension.UI
         public SnykFilterableTree()
         {
             InitializeComponent();
-
+            
             instance = this;
         }
 
@@ -269,6 +270,53 @@ namespace Snyk.VisualStudio.Extension.UI
             }
 
             return icon;
+        }
+    }
+
+    public class LeftMarginMultiplierConverter : IValueConverter
+    {
+        public double Length { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TreeViewItem item = value as TreeViewItem;
+
+            if (item == null)
+            {
+                return new Thickness(0);
+            }
+                
+            return new Thickness(Length * item.GetDepth(), 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public static class TreeViewItemExtensions
+    {
+        public static int GetDepth(this TreeViewItem item)
+        {
+            TreeViewItem parent;
+            while ((parent = GetParent(item)) != null)
+            {
+                return GetDepth(parent) + 1;
+            }
+            return 0;
+        }
+
+        private static TreeViewItem GetParent(TreeViewItem item)
+        {
+            DependencyObject parent = item != null ? VisualTreeHelper.GetParent(item) : null;
+
+            while (parent != null && !(parent is TreeViewItem || parent is TreeView))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as TreeViewItem;
         }
     }
 }
