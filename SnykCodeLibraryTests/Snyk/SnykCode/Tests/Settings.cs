6bff78ca1be284c1851ba8560d84faaf7f934d4a
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-06-06 16:50:04 +0300

    feat: add initil version of snyk code sessions support.

---
+++ SnykCodeLibraryTests/Snyk/SnykCode/Tests/Settings.cs
@@ -0,0 +1,56 @@
+﻿namespace Snyk.SnykCode.Tests
+{    
+    using System;
+    using System.IO;
+    using System.Text;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Test settings.
+    /// </summary>
+    public class Settings
+    {
+        private const string SettingsFileName = "settings.json";
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Api token.
+        /// </summary>
+        public string ApiToken { get; set; }
+
+        private static Settings instance;
+
+        /// <summary>
+        /// Gets a value indicating whether Settings instance. If settings not loaded it will load it first from settings.json.
+        /// </summary>
+        /// <returns><see cref="Settings"/> instance.</returns>
+        public static Settings Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = Load();
+                }
+
+                return instance;
+            }
+        }
+
+        private static Settings Load()
+        {
+            string settingsPath = Path.Combine(GetProjectPath(), SettingsFileName);
+
+            return JsonConvert
+                .DeserializeObject<Settings>(File.ReadAllText(settingsPath, Encoding.UTF8));
+        }
+
+        private static string GetProjectPath()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+
+            return Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        }
+    }
+}
---
+++ SnykCodeLibraryTests/Snyk/SnykCode/Tests/SnykCodeClientTest.cs
@@ -0,0 +1,46 @@
+﻿namespace Snyk.SnykCode.Tests
+{
+    using SnykCode;
+    using Xunit;
+
+    public class SnykCodeClientTest
+    {
+        private const string TestUserAgent = "Test-VisualStudio";
+
+        [Fact]
+        public void LoginSuccess()
+        {
+            var snykCodeClient = new SnykCodeClient();
+
+            LoginResponse response = snykCodeClient.LoginAsync(TestUserAgent).Result;
+
+            Assert.NotNull(response);
+            Assert.NotEmpty(response.sessionToken);
+        }
+
+        [Fact]
+        public void LoginFailed()
+        {
+            var snykCodeClient = new SnykCodeClient();
+
+            LoginResponse response = snykCodeClient.LoginAsync("\\{").Result;
+
+            Assert.False(response.IsSuccess);
+        }
+
+        // <summary>
+        // Not working right now.
+        // </summary>
+        [Fact]
+        public void CheckSessionSuccess()
+        {
+            var snykCodeClient = new SnykCodeClient();
+
+            _ = snykCodeClient.LoginAsync(TestUserAgent).Result;
+
+            LoginStatus status = snykCodeClient.CheckSessionAsync(Settings.Instance.ApiToken).Result;
+
+            Assert.True(status.IsSucccess);
+        }
+    }
+}
