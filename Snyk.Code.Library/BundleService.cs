6a5ac377928fea0dd8a754b1cdd404fe1d48063f
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-06-30 17:16:23 +0300

    fix: add analysers and fix issues.

--- Snyk.Code.Library/BundleService.cs
+++ Snyk.Code.Library/BundleService.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using System.Threading.Tasks;
     using Snyk.Code.Library.Api;
     using Snyk.Code.Library.Api.Dto;
     using Snyk.Code.Library.Common;
@@ -25,37 +26,37 @@
 
         /// <summary>
         /// Create new <see cref="BundleResponseDto"/> and get result <see cref="BundleResponseDto"/> object.
-        /// If payload < 4 Mb it just send this bundle and return results.
-        /// If payload > 4 Mb it will:
+        /// If payload &lt; 4 Mb it just send this bundle and return results.
+        /// If payload &gt; 4 Mb it will:
         ///      Split initial bundle on list of bundles (chunks).
         ///      Call Create bundle REST API for first bundle in list.
         ///      For all other bundles it will Extend bundle.
         ///      Return last bundle as result.
         /// </summary>
-        /// <param name="files">Files dictionary (file path - file hash) for new bundle.</param>
+        /// <param name="pathToHashFileDict">Files dictionary (file path - file hash) for new bundle.</param>
         /// <param name="maxChunkSize">Maximum allowed bundle size.</param>
         /// <returns>Bundle object with bundle id, missing files and upload url.</returns>
-        public async System.Threading.Tasks.Task<Bundle> CreateBundle(Dictionary<string, string> files, int maxChunkSize = SnykCodeClient.MaxBundleSize)
+        public async Task<Bundle> CreateBundleAsync(Dictionary<string, string> pathToHashFileDict, int maxChunkSize = SnykCodeClient.MaxBundleSize)
         {
-            if (files == null || files.IsNullOrEmpty())
+            if (pathToHashFileDict == null || pathToHashFileDict.IsNullOrEmpty())
             {
                 throw new ArgumentException("Files list is null or empty.");
             }
 
-            int payloadSize = this.CalculateFilesSize(files);
+            int payloadSize = this.CalculateFilesSize(pathToHashFileDict);
 
-            BundleResponseDto bundleDto = null;
+            Task<BundleResponseDto> bundleDto = null;
 
             if (payloadSize < maxChunkSize)
             {
-                bundleDto = await this.codeClient.CreateBundle(files);
+                bundleDto = this.codeClient.CreateBundleAsync(pathToHashFileDict);
             }
             else
             {
-                bundleDto = await this.ProcessCreateLargeBundle(files, maxChunkSize);
+                bundleDto = this.ProcessCreateLargeBundleAsync(pathToHashFileDict, maxChunkSize);
             }
 
-            return this.MapDtoBundleToDomain(bundleDto);
+            return this.MapDtoBundleToDomain(await bundleDto);
         }
 
         /// <summary>
@@ -63,14 +64,14 @@
         /// This method wrap functionality to extend bundle if it's small by size or make few chunks and extend by chunks.
         /// </summary>
         /// <param name="bundleId">Already created bundle id.</param>
-        /// <param name="files">Files to add in bundle.</param>
-        /// <param name="removedFiles">Files to remove in bundle.</param>
+        /// <param name="pathToHashFileDict">Files to add in bundle.</param>
+        /// <param name="filesToRemovePaths">Files to remove in bundle.</param>
         /// <param name="maxChunkSize">Maximum bundle chunk size. By default it is 4 Mb.</param>
         /// <returns>Result extended bundle.</returns>
-        public async System.Threading.Tasks.Task<Bundle> ExtendBundle(
+        public async Task<Bundle> ExtendBundleAsync(
             string bundleId,
-            Dictionary<string, string> files,
-            List<string> removedFiles,
+            Dictionary<string, string> pathToHashFileDict,
+            List<string> filesToRemovePaths,
             int maxChunkSize = SnykCodeClient.MaxBundleSize)
         {
             if (string.IsNullOrEmpty(bundleId))
@@ -78,39 +79,39 @@
                 throw new ArgumentException("Bundle id is null or empty.");
             }
 
-            if (files.IsNullOrEmpty() && removedFiles.IsNullOrEmpty())
+            if (pathToHashFileDict.IsNullOrEmpty() && filesToRemovePaths.IsNullOrEmpty())
             {
                 throw new ArgumentException("On Extend Bundle files or removed files are empty.");
             }
 
-            int payloadSize = this.CalculateFilesSize(files) + this.CalculateFilesSize(removedFiles);
+            int payloadSize = this.CalculateFilesSize(pathToHashFileDict) + this.CalculateFilesSize(filesToRemovePaths);
 
-            BundleResponseDto bundleDto = null;
+            Task<BundleResponseDto> bundleDto = null;
 
             // If payload < 4 max bundle chunk size just send this bundle and return results.
             if (payloadSize < maxChunkSize)
             {
-                bundleDto = await this.codeClient.ExtendBundle(bundleId, files, removedFiles);
+                bundleDto = this.codeClient.ExtendBundleAsync(bundleId, pathToHashFileDict, filesToRemovePaths);
             }
             else
             {
-                bundleDto = await this.ProcessExtendLargeBundle(bundleId, files, removedFiles, maxChunkSize);
+                bundleDto = this.ProcessExtendLargeBundleAsync(bundleId, pathToHashFileDict, filesToRemovePaths, maxChunkSize);
             }
 
-            return this.MapDtoBundleToDomain(bundleDto);
+            return this.MapDtoBundleToDomain(await bundleDto);
         }
 
         /// <summary>
         /// Split bundle to list of bundles by maximun bundle size.
         /// </summary>
-        /// <param name="files">Source files dictionary.</param>
+        /// <param name="pathToHashFileDict">Source files dictionary.</param>
         /// <param name="maxChunkSize">Maximum chunk size. By default it's 4 Mb.</param>
         /// <returns>List of smaller file dictionaries.</returns>
-        public List<Dictionary<string, string>> SplitFilesToChunkListsBySize(Dictionary<string, string> files, int maxChunkSize = SnykCodeClient.MaxBundleSize)
+        public List<Dictionary<string, string>> SplitFilesToChunkListsBySize(Dictionary<string, string> pathToHashFileDict, int maxChunkSize = SnykCodeClient.MaxBundleSize)
         {
             var fileDictionaries = new List<Dictionary<string, string>>();
 
-            if (files.IsNullOrEmpty())
+            if (pathToHashFileDict.IsNullOrEmpty())
             {
                 return fileDictionaries;
             }
@@ -119,7 +120,7 @@
 
             var fileDictionary = new Dictionary<string, string>();
 
-            foreach (var filePair in files)
+            foreach (var filePair in pathToHashFileDict)
             {
                 int fileSize = this.CalculateFilePairSize(filePair);
 
@@ -199,13 +200,13 @@
         /// Split big bundle to list of small bundles and extend bundle using this "chunk" bundles.
         /// </summary>
         /// <param name="bundleId">Already created bundle id to extend.</param>
-        /// <param name="files">Files data for extend.</param>
+        /// <param name="pathToHashFileDict">Files data for extend.</param>
         /// <param name="removedFiles">Removed files data for delete in extend bundle.</param>
         /// <param name="maxChunkSize">Maximum bundle size. By default it's 4 Mb.</param>
         /// <returns>Result Bundle object from server.</returns>
-        public async System.Threading.Tasks.Task<BundleResponseDto> ProcessExtendLargeBundle(
+        public async Task<BundleResponseDto> ProcessExtendLargeBundleAsync(
             string bundleId,
-            Dictionary<string, string> files,
+            Dictionary<string, string> pathToHashFileDict,
             List<string> removedFiles,
             int maxChunkSize = SnykCodeClient.MaxBundleSize)
         {
@@ -216,12 +217,12 @@
 
             if (!removedFiles.IsNullOrEmpty())
             {
-                bundleDto = await this.ExtendBundleWithRemovedFiles(bundleDto.Id, removedFiles, maxChunkSize);
+                bundleDto = await this.ExtendBundleWithRemovedFilesAsync(bundleDto.Id, removedFiles, maxChunkSize);
             }
 
-            if (!files.IsNullOrEmpty())
+            if (!pathToHashFileDict.IsNullOrEmpty())
             {
-                bundleDto = await this.ExtendBundleWithFiles(bundleDto.Id, files, maxChunkSize);
+                bundleDto = await this.ExtendBundleWithFilesAsync(bundleDto.Id, pathToHashFileDict, maxChunkSize);
             }
 
             return bundleDto;
@@ -231,23 +232,23 @@
         /// Extend (update) bundle with removed files.
         /// </summary>
         /// <param name="bundleId">Source bundle id to extend.</param>
-        /// <param name="files">Files to update extend bundle.</param>
+        /// <param name="pathToHashFileDict">Files to update extend bundle.</param>
         /// <param name="maxChunkSize">Maximum bundle size. By default it's 4 Mb.</param>
         /// <returns>Result Bundle object from server.</returns>
-        public async System.Threading.Tasks.Task<BundleResponseDto> ExtendBundleWithFiles(string bundleId, Dictionary<string, string> files, int maxChunkSize = SnykCodeClient.MaxBundleSize)
+        public async Task<BundleResponseDto> ExtendBundleWithFilesAsync(string bundleId, Dictionary<string, string> pathToHashFileDict, int maxChunkSize = SnykCodeClient.MaxBundleSize)
         {
-            var fileDictionaries = this.SplitFilesToChunkListsBySize(files, maxChunkSize);
+            var fileDictionaries = this.SplitFilesToChunkListsBySize(pathToHashFileDict, maxChunkSize);
 
             var firstFiles = fileDictionaries[0];
 
-            var resultBundleDto = await this.codeClient.ExtendBundle(bundleId, firstFiles, Enumerable.Empty<string>().ToList());
+            var resultBundleDto = await this.codeClient.ExtendBundleAsync(bundleId, firstFiles, Enumerable.Empty<string>().ToList());
 
             fileDictionaries.Remove(firstFiles);
 
             // Call Extend Bundle REST API for bundles.
             foreach (var filesDictionary in fileDictionaries)
             {
-                resultBundleDto = await this.codeClient.ExtendBundle(resultBundleDto.Id, filesDictionary, Enumerable.Empty<string>().ToList());
+                resultBundleDto = await this.codeClient.ExtendBundleAsync(resultBundleDto.Id, filesDictionary, Enumerable.Empty<string>().ToList());
             }
 
             // Last created bundle is result bundle.
@@ -258,24 +259,24 @@
         /// Extend (update) bundle with removed files.
         /// </summary>
         /// <param name="bundleId">Source bundle id to extend.</param>
-        /// <param name="removedFiles">Files to remove.</param>
+        /// <param name="filesToRemovePaths">Files to remove.</param>
         /// <param name="maxChunkSize">Maximum bundle size. By default it's 4 Mb.</param>
         /// <returns>Result Bundle object from server.</returns>
-        public async System.Threading.Tasks.Task<BundleResponseDto> ExtendBundleWithRemovedFiles(string bundleId, List<string> removedFiles, int maxChunkSize = SnykCodeClient.MaxBundleSize)
+        public async Task<BundleResponseDto> ExtendBundleWithRemovedFilesAsync(string bundleId, List<string> filesToRemovePaths, int maxChunkSize = SnykCodeClient.MaxBundleSize)
         {
-            var removeFileChunks = this.SplitRemovedFilesToChunkListsBySize(removedFiles, maxChunkSize);
+            var removeFileChunks = this.SplitRemovedFilesToChunkListsBySize(filesToRemovePaths, maxChunkSize);
 
             var firstRemovedFiles = removeFileChunks[0];
             var emptyDictionary = new Dictionary<string, string>();
 
-            var resultBundleDto = await this.codeClient.ExtendBundle(bundleId, emptyDictionary, firstRemovedFiles);
+            var resultBundleDto = await this.codeClient.ExtendBundleAsync(bundleId, emptyDictionary, firstRemovedFiles);
 
             removeFileChunks.Remove(firstRemovedFiles);
 
             // Call Extend Bundle REST API for bundles.
             foreach (var removeFilesList in removeFileChunks)
             {
-                resultBundleDto = await this.codeClient.ExtendBundle(resultBundleDto.Id, emptyDictionary, removeFilesList);
+                resultBundleDto = await this.codeClient.ExtendBundleAsync(resultBundleDto.Id, emptyDictionary, removeFilesList);
             }
 
             // Last created bundle is result bundle.
@@ -285,24 +286,24 @@
         /// <summary> ProcessCreateLargeBundle
         /// Split big bundle to list of small bundles and create new bundle on server using this "chunk" bundles.
         /// </summary>
-        /// <param name="files">Source files dictionary.</param>
+        /// <param name="pathToHashFileDict">Source files dictionary.</param>
         /// <param name="maxChunkSize">Maximum bundle size. By default it's 4 Mb.</param>
         /// <returns>Result Bundle object from server.</returns>
-        public async System.Threading.Tasks.Task<BundleResponseDto> ProcessCreateLargeBundle(Dictionary<string, string> files, int maxChunkSize = SnykCodeClient.MaxBundleSize)
+        public async Task<BundleResponseDto> ProcessCreateLargeBundleAsync(Dictionary<string, string> pathToHashFileDict, int maxChunkSize = SnykCodeClient.MaxBundleSize)
         {
-            var fileDictionaries = this.SplitFilesToChunkListsBySize(files, maxChunkSize);
+            var fileDictionaries = this.SplitFilesToChunkListsBySize(pathToHashFileDict, maxChunkSize);
 
             var firstFiles = fileDictionaries[0];
 
             // Call Create Bundle REST API for first bundle in list to create it on server.
-            BundleResponseDto resultBundleDto = await this.codeClient.CreateBundle(firstFiles);
+            BundleResponseDto resultBundleDto = await this.codeClient.CreateBundleAsync(firstFiles);
 
             fileDictionaries.Remove(firstFiles);
 
             // Call Extend Bundle REST API for bundles.
             foreach (var extendFiles in fileDictionaries)
             {
-                resultBundleDto = await this.codeClient.ExtendBundle(resultBundleDto.Id, extendFiles, Enumerable.Empty<string>().ToList());
+                resultBundleDto = await this.codeClient.ExtendBundleAsync(resultBundleDto.Id, extendFiles, Enumerable.Empty<string>().ToList());
             }
 
             // Last created bundle is result bundle.
@@ -314,21 +315,21 @@
         /// </summary>
         /// <param name="filePair">Source file pair (file path + file hash).</param>
         /// <returns>Size in bytys.</returns>
-        private int CalculateFilePairSize(KeyValuePair<string, string> filePair) => this.CalculatePayloadSize(Json.Serialize<KeyValuePair<string, string>>(filePair));
+        private int CalculateFilePairSize(KeyValuePair<string, string> filePair) => this.CalculatePayloadSize(Json.Serialize(filePair));
 
         /// <summary>
         /// Claculate file pairs size.
         /// </summary>
         /// <param name="files">Source dictionary with file info.</param>
         /// <returns>Size of dictionary.</returns>
-        private int CalculateFilesSize(Dictionary<string, string> files) => this.CalculatePayloadSize(Json.Serialize<Dictionary<string, string>>(files));
+        private int CalculateFilesSize(Dictionary<string, string> files) => this.CalculatePayloadSize(Json.Serialize(files));
 
         /// <summary>
         /// Claculate file pairs size.
         /// </summary>
         /// <param name="files">Source dictionary with file info.</param>
         /// <returns>Size of dictionary.</returns>
-        private int CalculateFilesSize(List<string> files) => this.CalculatePayloadSize(Json.Serialize<List<string>>(files));
+        private int CalculateFilesSize(List<string> files) => this.CalculatePayloadSize(Json.Serialize(files));
 
         /// <summary>
         /// Calculate bundle size in bytes.
@@ -344,20 +345,12 @@
         /// <returns>Result domain Bundle object.</returns>
         private Bundle MapDtoBundleToDomain(BundleResponseDto bundleDto)
         {
-            Bundle bundle = new Bundle
+            return new Bundle
             {
-                Id = string.Copy(bundleDto.Id),
-                UploadURL = string.Copy(bundleDto.UploadURL),
+                Id = bundleDto.Id,
+                UploadURL = bundleDto.UploadURL,
+                MissingFiles = bundleDto.MissingFiles,
             };
-
-            if (!bundleDto.MissingFiles.IsNullOrEmpty())
-            {
-                bundle.MissingFiles = new string[bundleDto.MissingFiles.Length];
-
-                bundleDto.MissingFiles.CopyTo(bundle.MissingFiles, 0);
-            }
-
-            return bundle;
         }
     }
 }
