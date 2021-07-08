4c051cf44b637c5bcec0a2c8fa3c113bc236b17a
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-07-08 11:23:09 +0300

    feat: add filter input files.

---
+++ Snyk.Code.Library.Tests/SnykCode/SnykCodeServiceTest.cs
@@ -0,0 +1,153 @@
+﻿namespace Snyk.Code.Library.Tests.Api
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Moq;
+    using Snyk.Code.Library.Api;
+    using Snyk.Code.Library.Api.Dto;
+    using Xunit;
+
+    /// <summary>
+    /// Test cases for <see cref="SnykCodeService"/>.
+    /// </summary>
+    public class SnykCodeServiceTest
+    {
+        [Fact]
+        public async Task SnykCodeService_FiveCodeFilesProvided_FilterFilesCheckPassAsync()
+        {
+            IList<string> files = new List<string>();
+
+            files.Add("/home/test/projects/main/app.js");
+            files.Add("/home/test/projects/main/app.js.dev");
+            files.Add("/home/test/projects/main/main.ts");
+            files.Add("C:\\projects\\main\\Db.cs");
+            files.Add("C:\\projects\\Db.cs.dev");
+
+            var codeClientMock = new Mock<ISnykCodeClient>();
+
+            var codeService = new SnykCodeService(codeClientMock.Object);
+
+            var extensions = new List<string>();
+            extensions.Add(".cs");
+            extensions.Add(".ts");
+            extensions.Add(".js");
+
+            var filtersDto = new FiltersDto
+            {
+                Extensions = extensions,
+                ConfigFiles = new List<string>(),
+            };
+
+            codeClientMock
+                .Setup(codeClient => codeClient.GetFiltersAsync().Result)
+                .Returns(filtersDto);
+
+            var filteredFiles = await codeService.FilterFilesAsync(files);
+
+            Assert.NotNull(filteredFiles);
+            Assert.Equal(3, filteredFiles.Count);
+
+            Assert.True(filteredFiles.Contains("/home/test/projects/main/app.js"));
+            Assert.True(filteredFiles.Contains("/home/test/projects/main/main.ts"));
+            Assert.True(filteredFiles.Contains("C:\\projects\\main\\Db.cs"));
+
+            codeClientMock.Verify(codeClient => codeClient.GetFiltersAsync());
+        }
+
+        [Fact]
+        public async Task SnykCodeService_FiveConfigFilesProvided_FilterFilesCheckPassAsync()
+        {
+            IList<string> files = new List<string>();
+
+            files.Add("/home/test/projects/main/.dcignore");
+            files.Add("/home/test/projects/main/.gitignore");
+            files.Add("/home/test/projects/main/main.ts.dev");
+            files.Add("C:\\projects\\main\\Db.cs.dev");
+            files.Add("C:\\projects\\.gitignore");
+
+            var codeClientMock = new Mock<ISnykCodeClient>();
+
+            var codeService = new SnykCodeService(codeClientMock.Object);
+
+            var configFiles = new List<string>();
+            configFiles.Add(".dcignore");
+            configFiles.Add(".gitignore");
+
+            var filtersDto = new FiltersDto
+            {
+                Extensions = new List<string>(),
+                ConfigFiles = configFiles,
+            };
+
+            codeClientMock
+                .Setup(codeClient => codeClient.GetFiltersAsync().Result)
+                .Returns(filtersDto);
+
+            var filteredFiles = await codeService.FilterFilesAsync(files);
+
+            Assert.NotNull(filteredFiles);
+            Assert.Equal(3, filteredFiles.Count);
+
+            Assert.True(filteredFiles.Contains("/home/test/projects/main/.dcignore"));
+            Assert.True(filteredFiles.Contains("/home/test/projects/main/.gitignore"));
+            Assert.True(filteredFiles.Contains("C:\\projects\\.gitignore"));
+
+            codeClientMock.Verify(codeClient => codeClient.GetFiltersAsync());
+        }
+
+        [Fact]
+        public async Task SnykCodeService_FveCodeFilesAndFiveConfigFilesProvided_FilterFilesCheckPassAsync()
+        {
+            IList<string> files = new List<string>();
+
+            files.Add("/home/test/projects/main/.dcignore");
+            files.Add("/home/test/projects/main/.gitignore");
+            files.Add("/home/test/projects/main/main.ts.dev");
+            files.Add("C:\\projects\\main\\Db.cs.dev");
+            files.Add("C:\\projects\\.gitignore");
+            files.Add("/home/test/projects/main/app.js");
+            files.Add("/home/test/projects/main/app.js.dev");
+            files.Add("/home/test/projects/main/main.ts");
+            files.Add("C:\\projects\\main\\Db.cs");
+            files.Add("C:\\projects\\Db.cs.dev");
+
+            var codeClientMock = new Mock<ISnykCodeClient>();
+
+            var codeService = new SnykCodeService(codeClientMock.Object);
+
+            var extensions = new List<string>();
+            extensions.Add(".cs");
+            extensions.Add(".ts");
+            extensions.Add(".js");
+
+            var configFiles = new List<string>();
+            configFiles.Add(".dcignore");
+            configFiles.Add(".gitignore");
+
+            var filtersDto = new FiltersDto
+            {
+                Extensions = extensions,
+                ConfigFiles = configFiles,
+            };
+
+            codeClientMock
+                .Setup(codeClient => codeClient.GetFiltersAsync().Result)
+                .Returns(filtersDto);
+
+            var filteredFiles = await codeService.FilterFilesAsync(files);
+
+            Assert.NotNull(filteredFiles);
+            Assert.Equal(6, filteredFiles.Count);
+
+            Assert.True(filteredFiles.Contains("/home/test/projects/main/app.js"));
+            Assert.True(filteredFiles.Contains("/home/test/projects/main/main.ts"));
+            Assert.True(filteredFiles.Contains("C:\\projects\\main\\Db.cs"));
+
+            Assert.True(filteredFiles.Contains("/home/test/projects/main/.dcignore"));
+            Assert.True(filteredFiles.Contains("/home/test/projects/main/.gitignore"));
+            Assert.True(filteredFiles.Contains("C:\\projects\\.gitignore"));
+
+            codeClientMock.Verify(codeClient => codeClient.GetFiltersAsync());
+        }
+    }
+}
---
+++ Snyk.Code.Library/SnykCodeService.cs
@@ -0,0 +1,58 @@
+﻿namespace Snyk.Code.Library
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Snyk.Code.Library.Api;
+    using Snyk.Code.Library.Api.Dto;
+
+    /// <summary>
+    /// Contains gigh level busines logic for SnykCode APIs.
+    /// </summary>
+    public class SnykCodeService
+    {
+        private ISnykCodeClient codeClient;
+
+        private FiltersDto filters;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SnykCodeService"/> class.
+        /// </summary>
+        /// <param name="snykCodeClient">SnykCode client implementation.</param>
+        public SnykCodeService(ISnykCodeClient snykCodeClient) => this.codeClient = snykCodeClient;
+
+        /// <summary>
+        /// Get filters information from server if it's not initialized yet and filter file pahts with supported extensions and configuration files.
+        /// </summary>
+        /// <param name="filePaths">Input file paths to filter.</param>
+        /// <returns>Filtered file pahts (only supported by SnykCode files by extension).</returns>
+        public async Task<IList<string>> FilterFilesAsync(IList<string> filePaths)
+        {
+            var filters = await this.GetFiltersAsync();
+            var extensionFilters = filters.Extensions;
+            var configFileFilters = filters.ConfigFiles;
+
+            var filteredFiles = new List<string>();
+
+            foreach (string filePath in filePaths)
+            {
+                if (extensionFilters.Contains(Path.GetExtension(filePath)) || configFileFilters.Contains(Path.GetFileName(filePath)))
+                {
+                    filteredFiles.Add(filePath);
+                }
+            }
+
+            return filteredFiles;
+        }
+
+        private async Task<FiltersDto> GetFiltersAsync()
+        {
+            if (this.filters == null)
+            {
+                this.filters = await this.codeClient.GetFiltersAsync();
+            }
+
+            return this.filters;
+        }
+    }
+}
