5d32819879453c14a510a63d8f0b08d71b577227
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-09-14 20:35:27 +0300

    feat: add snyk code enable logic.

--- Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Settings/SnykGeneralSettingsUserControl.cs
+++ Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Settings/SnykGeneralSettingsUserControl.cs
@@ -32,8 +32,6 @@
 
         private SnykActivityLogger logger;
 
-        private static readonly int MaxSastRequestAttempts = 20;
-
         private SnykApiService apiService;
 
         private Timer snykCodeEnableTimer = new Timer();
@@ -190,7 +188,7 @@
             {
                 var serviceProvider = this.OptionsDialogPage.ServiceProvider;
                 var tasksService = serviceProvider.TasksService;
-
+                
                 if (SnykCli.IsCliExists())
                 {
                     Logger.Information("CLI exists. Calling SetupApiToken method");
