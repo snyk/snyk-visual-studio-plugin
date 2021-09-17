8503bf5b78ad38b6e040e05efc6a840efb447454
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-09-17 18:28:42 +0300

    fix: display description panel and tree node status.

--- Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Settings/SnykGeneralSettingsUserControl.cs
+++ Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Settings/SnykGeneralSettingsUserControl.cs
@@ -353,6 +353,8 @@
 
         private void UpdateSnykCodeEnablementSettings(bool snykCodeEnabled)
         {
+            bool snykCodeEnabled = await this.apiService.IsSnyCodeEnabledAsync();
+
             this.codeSecurityEnabledCheckBox.Enabled = snykCodeEnabled;
             this.codeQualityEnabledCheckBox.Enabled = snykCodeEnabled;
 
