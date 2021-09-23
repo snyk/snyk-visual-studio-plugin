e3a3ca3b8c22dbd460989c31704475fea8bedcb2
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-09-23 06:29:46 +0300

    chore: rebase.

--- Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Settings/SnykGeneralSettingsUserControl.cs
+++ Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Settings/SnykGeneralSettingsUserControl.cs
@@ -30,6 +30,10 @@
 
         private static readonly int MaxSastRequestAttempts = 20;
 
+        private SnykActivityLogger logger;
+
+        private static readonly int MaxSastRequestAttempts = 20;
+
         private SnykApiService apiService;
 
         private Timer snykCodeEnableTimer = new Timer();
@@ -186,7 +190,7 @@
             {
                 var serviceProvider = this.OptionsDialogPage.ServiceProvider;
                 var tasksService = serviceProvider.TasksService;
-                
+
                 if (SnykCli.IsCliExists())
                 {
                     Logger.Information("CLI exists. Calling SetupApiToken method");
@@ -359,6 +363,45 @@
             this.snykCodeSettingsLinkLabel.Visible = !snykCodeEnabled;
             this.checkAgainLinkLabel.Visible = !snykCodeEnabled;
         }
+
+        private async Task StartSastEnablementCheckLoopAsync()
+        {
+            if (this.snykCodeEnableTimer.Enabled)
+            {
+                this.snykCodeEnableTimer.Stop();
+            }
+
+            bool onServerSnykCodeEnabled = await this.apiService.IsSnykCodeEnabledAsync();
+
+            this.UpdateSnykCodeEnablementSettings(onServerSnykCodeEnabled);
+
+            if (!onServerSnykCodeEnabled)
+            {
+                int currentRequestAttempt = 1;
+
+                this.snykCodeEnableTimer.Interval = TwoSecondsDelay;
+
+                this.snykCodeEnableTimer.Tick += async (sender, eventArgs) =>
+                {
+                    bool snykCodeEnabled = await this.apiService.IsSnykCodeEnabledAsync();
+
+                    this.UpdateSnykCodeEnablementSettings(snykCodeEnabled);
+
+                    if (snykCodeEnabled)
+                    {
+                        this.snykCodeEnableTimer.Stop();
+                    }
+                    else if (currentRequestAttempt < MaxSastRequestAttempts)
+                    {
+                        currentRequestAttempt++;
+
+                        this.snykCodeEnableTimer.Interval = TwoSecondsDelay * currentRequestAttempt;
+                    }
+                    else
+                    {
+                        this.snykCodeEnableTimer.Stop();
+                    }
+                };
 
         private async Task StartSastEnablementCheckLoopAsync()
         {
