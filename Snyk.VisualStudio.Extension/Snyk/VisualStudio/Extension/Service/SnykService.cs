a4f50adfb1a7a1a342c0e2acc4996a71bf8b79b6
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-09-23 10:11:08 +0300

    fix: rebase.

--- Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Service/SnykService.cs
+++ Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Service/SnykService.cs
@@ -160,6 +160,20 @@
             }
         }
 
+        /// <inheritdoc/>
+        public SnykApiService ApiService
+        {
+            get
+            {
+                if (this.apiService == null)
+                {
+                    this.apiService = new SnykApiService(this.Options);
+                }
+
+                return this.apiService;
+            }
+        }
+
         /// <summary>
         /// Show Snyk tool window.
         /// </summary>
