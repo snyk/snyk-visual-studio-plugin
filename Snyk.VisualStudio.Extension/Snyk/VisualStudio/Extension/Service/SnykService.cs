b9aeac7b579eff467dd7e204843a21110c67b7cf
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-09-23 10:14:30 +0300

    fix: rebase.

--- Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Service/SnykService.cs
+++ Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/Service/SnykService.cs
@@ -171,6 +171,8 @@
                 if (this.apiService == null)
                 {
                     this.apiService = new SnykApiService(this.Options);
+
+                    this.Options.SettingsChanged += this.OnSettingsChanged;
                 }
 
                 return this.apiService;
