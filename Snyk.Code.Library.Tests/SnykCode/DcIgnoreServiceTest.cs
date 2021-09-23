d18451d2234eb16a82de722f1a68b05d9145d335
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-09-23 10:04:14 +0300

    feat: add filtering by gitignore and dcignore

---
+++ Snyk.Code.Library.Tests/SnykCode/DcIgnoreServiceTest.cs
@@ -0,0 +1,91 @@
+﻿namespace Snyk.Code.Library.Tests.SnykCode
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using Snyk.Code.Library.Service;
+    using Snyk.Code.Library.Tests.Api;
+    using Xunit;
+
+    /// <summary>
+    /// Tests for <see cref="IDcIgnoreService"/>.
+    /// </summary>
+    public class DcIgnoreServiceTest
+    {
+        [Fact]
+        public void FiltersService_ListOfProjectFilesProvided_CreateDcIgnoreFileAndFilterFilesPass()
+        {
+            string folderPath = Path.GetTempPath();
+
+            string dcIGnorePath = Path.Combine(folderPath, ".dcignore");
+
+            if (File.Exists(dcIGnorePath))
+            {
+                File.Delete(dcIGnorePath);
+            }
+
+            Assert.False(File.Exists(dcIGnorePath));
+
+            var projectFiles = new List<string>
+            {
+                "/Bin/Main.cs",
+                "/bin/Main.cs",
+                "/Src/Main.cs",
+                "/.vs/App.cs",
+                "/DocProject/Help/html/index.js",
+                "/App.cs",
+                "/build/Service.cs",
+            };
+
+            var dcIgnoreService = new DcIgnoreService(folderPath);
+            var filteredFiles = dcIgnoreService.FilterFiles(projectFiles);
+
+            Assert.Equal(3, filteredFiles.Count);
+
+            Assert.True(File.Exists(dcIGnorePath));
+
+            File.Delete(dcIGnorePath);
+        }
+
+        [Fact]
+        public void FiltersService_SolutionWithoutDcIgnoreProvided_CreateDcPass()
+        {
+            string folderPath = Path.GetTempPath();
+
+            string dcIGnorePath = Path.Combine(folderPath, ".dcignore");
+
+            if (File.Exists(dcIGnorePath))
+            {
+                File.Delete(dcIGnorePath);
+            }
+
+            Assert.False(File.Exists(dcIGnorePath));
+
+            var dcIgnoreService = new DcIgnoreService(folderPath);
+            dcIgnoreService.CreateDcIgnoreIfNeeded();
+
+            Assert.True(File.Exists(dcIGnorePath));
+
+            File.Delete(dcIGnorePath);
+        }
+
+        [Fact]
+        public void FiltersService_ListOfProjectFilesProvided_FilterFilesByGitIgnoreCheckPass()
+        {
+            var projectFiles = new List<string>
+            {
+                "/Bin/Main.cs",
+                "/bin/Main.cs",
+                "/Src/Main.cs",
+                "/.vs/App.cs",
+                "/DocProject/Help/html/index.js",
+                "/App.cs",
+                "/build/Service.cs",
+            };
+
+            var dcIgnoreService = new DcIgnoreService(TestResource.GetResourcesPath());
+            var filteredFiles = dcIgnoreService.FilterFilesByGitIgnore(projectFiles);
+
+            Assert.Equal(4, filteredFiles.Count);
+        }
+    }
+}
---
+++ Snyk.Code.Library/Service/DcIgnoreService.cs
@@ -0,0 +1,80 @@
+﻿namespace Snyk.Code.Library.Service
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using MAB.DotIgnore;
+
+    /// <inheritdoc/>
+    public class DcIgnoreService : IDcIgnoreService
+    {
+        private string folderPath;
+
+        private string gitIGnorePath;
+        private string dcIGnorePath;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DcIgnoreService"/> class.
+        /// </summary>
+        /// <param name="folderPath">Basic folder path.</param>
+        public DcIgnoreService(string folderPath)
+        {
+            this.folderPath = folderPath;
+
+            this.gitIGnorePath = Path.Combine(this.folderPath, ".gitignore");
+            this.dcIGnorePath = Path.Combine(this.folderPath, ".dcignore");
+        }
+
+        /// <inheritdoc/>
+        public void CreateDcIgnoreIfNeeded()
+        {
+            if (!File.Exists(this.gitIGnorePath) && !File.Exists(this.dcIGnorePath))
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                string resourcePath = assembly.GetManifestResourceNames()
+                        .Single(str => str.EndsWith("full.dcignore"));
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourcePath))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string dcIgnoreTemplate = reader.ReadToEnd();
+
+                        File.WriteAllText(this.dcIGnorePath, dcIgnoreTemplate);
+                    }
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public IList<string> FilterFiles(IList<string> filePaths)
+        {
+            this.CreateDcIgnoreIfNeeded();
+
+            var filteredFiles = this.FilterFilesByGitIgnore(filePaths);
+
+            return this.FilterFilesByDcIgnore(filteredFiles);
+        }
+
+        /// <inheritdoc/>
+        public IList<string> FilterFilesByDcIgnore(IList<string> filePaths)
+            => this.FilterFilesByIgnoreFile(this.dcIGnorePath, filePaths);
+
+        /// <inheritdoc/>
+        public IList<string> FilterFilesByGitIgnore(IList<string> filePaths)
+            => this.FilterFilesByIgnoreFile(this.gitIGnorePath, filePaths);
+
+        private IList<string> FilterFilesByIgnoreFile(string ignoreFilePath, IList<string> filePaths)
+        {
+            if (!File.Exists(ignoreFilePath))
+            {
+                return filePaths;
+            }
+
+            var ignores = new IgnoreList(ignoreFilePath);
+
+            return filePaths.Where(path => !ignores.IsIgnored(new FileInfo(path))).ToList();
+        }
+    }
+}
