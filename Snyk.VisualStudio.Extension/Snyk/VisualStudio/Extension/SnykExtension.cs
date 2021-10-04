40de4d444e05ec0060e63bd78930fea253116d0d
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-10-04 16:14:15 +0300

    fix: integration name.

--- Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/SnykExtension.cs
+++ Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/SnykExtension.cs
@@ -16,7 +16,7 @@
         /// <summary>
         /// Integration name.
         /// </summary>
-        public const string IntegrationName = "VISUAL_STUDIO_PREVIEW";
+        public const string IntegrationName = "VISUAL_STUDIO";
 
         private const string AppSettingsFileName = "appsettings.json";
 
