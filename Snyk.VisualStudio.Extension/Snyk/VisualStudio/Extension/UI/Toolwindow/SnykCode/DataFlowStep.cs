d9019bf7f7c2d2c4b153ffa2e80b6f9ea50b5ade
Aleksandr Danchenko <alex.danchenko@snyk.io>
2021-10-18 12:47:32 +0300

    fix: simplify to use linq with distinct

--- Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/UI/Toolwindow/SnykCode/DataFlowStep.cs
+++ Snyk.VisualStudio.Extension/Snyk/VisualStudio/Extension/UI/Toolwindow/SnykCode/DataFlowStep.cs
@@ -26,5 +26,33 @@
         /// Gets or sets navigation command.
         /// </summary>
         public ICommand NavigateCommand { get; set; }
+
+        /// <summary>
+        /// Compare by file name and line content.
+        /// </summary>
+        /// <param name="obj">Object to compare.</param>
+        /// <returns>True if file name and line content are equal.</returns>
+        public override bool Equals(object obj) => this.Equals(obj as DataFlowStep);
+
+        /// <summary>
+        /// Get hash code using fileName and lineContent.
+        /// </summary>
+        /// <returns>Int hash value.</returns>
+        public override int GetHashCode() => (this.FileName, this.LineContent).GetHashCode();
+
+        private bool Equals(DataFlowStep dataFlowStep)
+        {
+            if (dataFlowStep is null)
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, dataFlowStep))
+            {
+                return true;
+            }
+
+            return (this.FileName == dataFlowStep.FileName) && (this.LineContent == dataFlowStep.LineContent);
+        }
     }
 }
