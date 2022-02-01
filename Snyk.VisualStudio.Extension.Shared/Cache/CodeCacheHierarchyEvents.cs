0c76df7542a3f20212631485efce3fc4a1cc83f5
Aleksandr <alex.danchenko@snyk.io>
2022-02-01 11:53:52 +0200

    [ROAD-322] Feat/add oss cache (#83)
    feat: add oss cache

---
+++ Snyk.VisualStudio.Extension.Shared/Cache/CodeCacheHierarchyEvents.cs
@@ -0,0 +1,33 @@
+﻿namespace Snyk.VisualStudio.Extension.Shared.Cache
+{
+    using Microsoft.VisualStudio.Shell.Interop;
+    using Snyk.Common;
+
+    /// <summary>
+    /// Impleemnts <see cref="IVsHierarchyEvents"/> for SnykCode cache.
+    /// </summary>
+    public class CodeCacheHierarchyEvents : AbstractCacheHierarchyEvents
+    {
+        private IFileProvider fileProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeCacheHierarchyEvents"/> class.
+        /// </summary>
+        /// <param name="vsHierarchy">VS Hierarchy instance.</param>
+        /// <param name="fileProvider">File provider instance.</param>
+        public CodeCacheHierarchyEvents(IVsHierarchy vsHierarchy, IFileProvider fileProvider)
+            : base(vsHierarchy) => this.fileProvider = fileProvider;
+
+        /// <inheritdoc/>
+        public override void OnFileAdded(string filePath)
+            => this.fileProvider.AddChangedFile(filePath);
+
+        /// <inheritdoc/>
+        public override void OnFileDeleted(string filePath)
+            => this.fileProvider.RemoveFile(filePath);
+
+        /// <inheritdoc/>
+        public override void OnFileChanged(string filePath)
+            => this.fileProvider.AddChangedFile(filePath);
+    }
+}
