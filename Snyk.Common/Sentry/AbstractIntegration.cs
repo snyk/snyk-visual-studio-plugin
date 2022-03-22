2afe1efab243e81ea59153d279189ebd51bc7c6b
Aleksandr Danchenko <alex.danchenko@snyk.io>
2022-03-22 12:22:01 +0200

    feat: add tests for Sentry integrations

--- Snyk.Common/Sentry/AbstractIntegration.cs
+++ Snyk.Common/Sentry/AbstractIntegration.cs
@@ -27,21 +27,11 @@
         public AbstractIntegration() => this.appDomain = AppDomainAdapter.Instance;
 
         /// <summary>
-        /// Registers this integration with the hub.
-        /// </summary>
-        /// <remarks>
-        /// This method is invoked when the Hub is created.
-        /// </remarks>
-        /// <param name="hub">The hub.</param>
-        /// <param name="options">The options.</param>
-        public abstract void Register(IHub hub, SentryOptions options);
-
-        /// <summary>
         /// Check is provided exception stack trace contains mentions about extension.
         /// </summary>
         /// <param name="e">Source exception.</param>
         /// <returns>True if contains mentions about Snyk extension.</returns>
-        protected static bool IsNeedToHandleException(Exception e)
+        public static bool IsNeedToHandleException(Exception e)
         {
             if (IsExceptionRelatedToExtension(e))
             {
@@ -51,6 +41,16 @@
             return e is AggregateException aggregateEx
                 && aggregateEx.Flatten().InnerExceptions.Where(ex => IsExceptionRelatedToExtension(ex)).Any();
         }
+
+        /// <summary>
+        /// Registers this integration with the hub.
+        /// </summary>
+        /// <remarks>
+        /// This method is invoked when the Hub is created.
+        /// </remarks>
+        /// <param name="hub">The hub.</param>
+        /// <param name="options">The options.</param>
+        public abstract void Register(IHub hub, SentryOptions options);
 
         private static bool IsExceptionRelatedToExtension(Exception e)
             => e != null && e.StackTrace != null && e.StackTrace.IndexOf(SnykKey, StringComparison.OrdinalIgnoreCase) >= 0;
