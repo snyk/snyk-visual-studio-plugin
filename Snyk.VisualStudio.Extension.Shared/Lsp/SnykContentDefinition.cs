94cad9ad5ff80c2f07689d5ea37daa3e28187f76
Aleksandr Danchenko <alex.danchenko@snyk.io>
2022-04-29 11:28:50 +0300

    feat: initial version of lsp intergration

---
+++ Snyk.VisualStudio.Extension.Shared/Lsp/SnykContentDefinition.cs
@@ -0,0 +1,35 @@
+﻿namespace Snyk.VisualStudio.Extension.Shared.Lsp
+{
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.LanguageServer.Client;
+    using Microsoft.VisualStudio.Utilities;
+
+    public class SnykContentDefinition
+    {
+        public const string Identifier = "snyk";
+
+        public const string TfExtension = ".tf";
+        public const string CsExtension = ".cs";
+        public const string JsExtension = ".js";
+
+        [Export]
+        [Name(Identifier)]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition SnykContentTypeDefinition { get; set; }
+
+        [Export]
+        [FileExtension(TfExtension)]
+        [ContentType(Identifier)]
+        internal static FileExtensionToContentTypeDefinition SnykTfFileExtensionDefinition { get; set; }
+
+        [Export]
+        [FileExtension(CsExtension)]
+        [ContentType(Identifier)]
+        internal static FileExtensionToContentTypeDefinition SnykCsFileExtensionDefinition { get; set; }
+
+        [Export]
+        [FileExtension(JsExtension)]
+        [ContentType(Identifier)]
+        internal static FileExtensionToContentTypeDefinition SnykJsFileExtensionDefinition { get; set; }
+    }
+}
---
+++ Snyk.VisualStudio.Extension.Shared/Lsp/SnykLanguageClient.cs
@@ -0,0 +1,77 @@
+﻿namespace Snyk.VisualStudio.Extension.Shared.Lsp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.LanguageServer.Client;
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Threading;
+    using Microsoft.VisualStudio.Utilities;
+    using Snyk.Common;
+
+    [ContentType(SnykContentDefinition.Identifier)]
+    [Export(typeof(ILanguageClient))]
+    public class SnykLanguageClient : ILanguageClient
+    {
+        public event AsyncEventHandler<EventArgs> StartAsync;
+
+        public event AsyncEventHandler<EventArgs> StopAsync;
+
+        public string Name => "Snyk Language Extension";
+
+        public IEnumerable<string> ConfigurationSections => null;
+
+        public object InitializationOptions => null;
+
+        public IEnumerable<string> FilesToWatch => null;
+
+        public bool ShowNotificationOnInitializeFailed => true;
+
+        public async Task<Connection> ActivateAsync(CancellationToken token)
+        {
+            await Task.Yield();
+
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Path.Combine(SnykExtension.GetExtensionDirectoryPath(), "snyk-ls.exe"),
+                    Arguments = string.Empty,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                },
+            };
+
+            return process.Start() ? new Connection(process.StandardOutput.BaseStream, process.StandardInput.BaseStream) : null;
+        }
+
+        public async Task OnLoadedAsync()
+        {
+            await this.StartAsync.InvokeAsync(this, EventArgs.Empty);
+        }
+
+        public Task OnServerInitializeFailedAsync(Exception e)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnServerInitializedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<InitializationFailureContext> OnServerInitializeFailedAsync(ILanguageClientInitializationInfo initializationState)
+        {
+            return Task.FromResult(new InitializationFailureContext()
+            {
+                FailureMessage = $"Language server failed to initialize during the {initializationState.Status} Stage",
+            });
+        }
+    }
+}
