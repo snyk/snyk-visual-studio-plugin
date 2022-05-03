8641b3ebe5bcad86a5cce3ebdd6f69a3d1891ebd
Asaf Agami <asaf.agami@snyk.io>
2022-05-03 12:54:55 +0300

    refactor: Add appsettings.development.json to csproj

--- Snyk.Common/SnykExtension.cs
+++ Snyk.Common/SnykExtension.cs
@@ -16,7 +16,7 @@
         /// </summary>
         public const string IntegrationName = "VISUAL_STUDIO";
 #if DEBUG
-        private const string AppSettingsFileName = "appsettings.Development.json";
+        private const string AppSettingsFileName = "appsettings.development.json";
 #else
         private const string AppSettingsFileName = "appsettings.json";
 #endif
