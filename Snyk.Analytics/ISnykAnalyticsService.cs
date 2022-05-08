90fc4842ed9cfaf4598e91237a7ae2f911b741aa
Asaf Agami <asaf.agami@snyk.io>
2022-05-08 12:19:33 +0300

    refactor: add ISnykAnalyticsService.cs

---
+++ Snyk.Analytics/ISnykAnalyticsService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Snyk.Analytics
+{
+    public interface ISnykAnalyticsService
+    {
+        /// <summary>
+        /// Log Analysis Is Ready event.
+        /// </summary>
+        /// <param name="analysisType">Type of analysis (Oss, SnykCode Security or quality).</param>
+        /// <param name="analysisResult">Analysis result (success or error).</param>
+        void LogAnalysisReadyEvent(string analysisType, string analysisResult);
+
+        /// <summary>
+        /// Log UserLandedOnTheWelcomePageEvent.
+        /// </summary>
+        void LogWelcomeIsViewedEvent();
+
+        /// <summary>
+        /// Log UserTriggersAnAnalysisEvent.
+        /// </summary>
+        /// <param name="selectedProducts">Selected products (OSS, SnykCode Quality and Security).</param>
+        void LogAnalysisIsTriggeredEvent(IList<string> selectedProducts);
+
+        /// <summary>
+        /// Log Issue Is Viewed Event.
+        /// </summary>
+        /// <param name="id">User id.</param>
+        /// <param name="issueType">Type of issue (Oss, SnykCode Security or Queality.</param>
+        /// <param name="severity">Severity name.</param>
+        void LogIssueIsViewedEvent(string id, string issueType, string severity);
+    }
+}
---
+++ Snyk.Analytics/SnykAnalyticsClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Snyk.Analytics
+{
+    public class SnykAnalyticsClient : ISnykAnalyticsService
+    {
+        public static async Task<SnykAnalyticsClient> CreateAsync()
+        {
+            await Task.Delay(1);
+            return null;
+        }
+
+        public void LogAnalysisReadyEvent(string analysisType, string analysisResult)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWelcomeIsViewedEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogAnalysisIsTriggeredEvent(IList<string> selectedProducts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogIssueIsViewedEvent(string id, string issueType, string severity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
