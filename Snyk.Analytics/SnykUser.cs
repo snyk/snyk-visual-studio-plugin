0a7cef60caad91fb05a4cbee7c0d751cf0fbedbb
Asaf Agami <asaf.agami@snyk.io>
2022-05-18 11:27:48 +0300

    chore: move using directives inside namespace

--- Snyk.Analytics/SnykUser.cs
+++ Snyk.Analytics/SnykUser.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Snyk.Common;
-
-namespace Snyk.Analytics
+﻿namespace Snyk.Analytics
 {
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using Snyk.Common;
+
     /// <summary>
     /// User for Snyk Analytics.
     /// </summary>
