7e83f278b96ace6b7734eb5128e29b3429c18446
Asaf Agami <asaf.agami@snyk.io>
2022-06-26 13:40:52 +0300

    refactor: rename Class1 to ExtensionStartupTests

--- Tests/MsVsIntegrationTests/SnykVsIntegrationTests/ExtensionStartupTests.cs
+++ Tests/MsVsIntegrationTests/SnykVsIntegrationTests/ExtensionStartupTests.cs
@@ -2,15 +2,15 @@
 {
 	using Xunit;
 
-	public class Class1
+	public class ExtensionStartupTests
 	{
-		[IdeFact(MinVersion = VisualStudioVersion.VS2022)]
+		[IdeFact]
 		public void Nothing_Test()
 		{
 			Assert.Equal(0, 0);
 		}
 
-		[IdeFact(MinVersion = VisualStudioVersion.VS2022)]
+		[IdeFact]
 		public void Nothing_Test_2()
 		{
 			Assert.Equal(1, 1);
