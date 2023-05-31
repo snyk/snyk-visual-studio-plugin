91aa0e5d0a70017737ebb90d7c17a34c58e517e5
Asaf Agami <asaf.agami@snyk.io>
2023-05-31 16:57:42 +0300

    fix: optimize IEnumerable.IsNullOrEmpty

--- Snyk.Common/CollectionExtensions.cs
+++ Snyk.Common/CollectionExtensions.cs
@@ -9,10 +9,10 @@
     public static class CollectionExtensions
     {
         /// <summary>
-        /// Check is collection is empty (items count is 0).
+        /// Check is collection is empty (items count is 0) or null.
         /// </summary>
         /// <param name="collection">Source collection for check.</param>
         /// <returns>True if collection is empty.</returns>
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection) => collection == null || collection.Count() == 0;
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection) => collection?.Any() == true;
     }
 }
