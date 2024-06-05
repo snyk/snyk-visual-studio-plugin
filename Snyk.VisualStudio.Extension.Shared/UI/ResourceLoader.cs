b86f8553b6580362383ae3197a93f0987a105eff
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2024-06-05 08:21:08 +0200

    chore: add ResourceLoader tests

--- Snyk.VisualStudio.Extension.Shared/UI/ResourceLoader.cs
+++ Snyk.VisualStudio.Extension.Shared/UI/ResourceLoader.cs
@@ -6,36 +6,40 @@ namespace Snyk.VisualStudio.Extension
     public static class ResourceLoader
     {
         private static string _resourceBasePath;
+        // ReSharper disable InconsistentNaming
+        private static readonly string _assemblyName = "Snyk.VisualStudio.Extension";
+        private static readonly string _resourcesDirectory = "/Resources";
+        // ReSharper restore InconsistentNaming
 
         public static string GetResourcePath(string imageName)
         {
-            var resourceBasePath = GetBaseResourcePath(imageName);
+            var resourceBasePath = GetBaseResourcePath(_assemblyName, _resourcesDirectory, imageName);
             if (string.IsNullOrEmpty(imageName))
-                throw new ArgumentNullException("imageName is null");
+                throw new ArgumentNullException(nameof(imageName));
             if (string.IsNullOrEmpty(resourceBasePath))
                 throw new ArgumentException($"Image with Path {imageName} not found");
             
             return resourceBasePath + imageName;
         }
 
-        public static string GetBaseResourcePath(string imageName)
+        public static string GetBaseResourcePath(string assemblyName, string resourcesDirectory, string imageName)
         {
             if(!string.IsNullOrEmpty(_resourceBasePath))
                 return _resourceBasePath;
 
-            var uri = $"pack://application:,,,/Snyk.VisualStudio.Extension;component/{imageName}";
+            var uri = $"pack://application:,,,/{assemblyName};component/{imageName}";
             if (ResourceExists(uri))
             {
                 _resourceBasePath =  uri.Replace(imageName, string.Empty);
                 return _resourceBasePath;
             }
 
-            var uriWithResources = $"pack://application:,,,/Snyk.VisualStudio.Extension;component/Resources/{imageName}";
+            var uriWithResources = $"pack://application:,,,/{assemblyName};component{resourcesDirectory}/{imageName}";
             _resourceBasePath =  ResourceExists(uriWithResources) ? uriWithResources.Replace(imageName, string.Empty) : null;
             
             return _resourceBasePath;
         }
-
+        
         private static bool ResourceExists(string uri)
         {
             try
