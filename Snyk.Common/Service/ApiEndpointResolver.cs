23f2f7ca2292b01ff95b8f01cac30ef3fa573085
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2024-07-31 12:12:28 +0200

    fix: fedramp code url

--- Snyk.Common/Service/ApiEndpointResolver.cs
+++ Snyk.Common/Service/ApiEndpointResolver.cs
@@ -60,8 +60,18 @@ namespace Snyk.Common.Service
 
             var endpoint = ResolveCustomEndpoint(this.options.CustomEndpoint);
 
-            var result = GetCustomEndpointUrlFromSnykApi(endpoint, "deeproxy");
-            
+            var isFedramp = this.options.IsFedramp();
+
+            if (isFedramp && string.IsNullOrEmpty(this.options.Organization))
+                throw new InvalidOperationException("Organization is required in a fedramp environment");
+
+            var subDomain = isFedramp ? "api" : "deeproxy";
+
+            var result = GetCustomEndpointUrlFromSnykApi(endpoint, subDomain);
+
+            if (isFedramp)
+                result += $"/hidden/orgs/{this.options.Organization}/code";
+
             return result + "/";
         }
 
