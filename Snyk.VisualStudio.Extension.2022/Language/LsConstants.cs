187cff3f5d49f37061a4d31f7f18e32383b79f37
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2024-10-09 12:52:24 +0200

    chore: bump ls protocol version to 16

--- Snyk.VisualStudio.Extension.2022/Language/LsConstants.cs
+++ Snyk.VisualStudio.Extension.2022/Language/LsConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class LsConstants
     {
-        public const string ProtocolVersion = "15";
+        public const string ProtocolVersion = "16";
         
         // Notifications
         public const string SnykHasAuthenticated = "$/snyk.hasAuthenticated";
