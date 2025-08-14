531706763ed1090ea2c43b1f0daab5586dc234eb
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-08-14 17:05:20 +0200

    chore: order imports

--- Snyk.VisualStudio.Extension.2022/Language/SnykLanguageClientCustomTarget.cs
+++ Snyk.VisualStudio.Extension.2022/Language/SnykLanguageClientCustomTarget.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Threading;
-using Newtonsoft.Json.Linq;
-using Snyk.VisualStudio.Extension.Authentication;
-using Snyk.VisualStudio.Extension.Extension;
-using Snyk.VisualStudio.Extension.Service;
-using StreamJsonRpc;
-using System;
+﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Threading;
+using Newtonsoft.Json.Linq;
+using StreamJsonRpc;
+using Snyk.VisualStudio.Extension.Authentication;
+using Snyk.VisualStudio.Extension.Extension;
+using Snyk.VisualStudio.Extension.Service;
 
 namespace Snyk.VisualStudio.Extension.Language
 {
