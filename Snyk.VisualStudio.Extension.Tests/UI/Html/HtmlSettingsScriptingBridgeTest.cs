02d76019383933593d1f0d9e42307dfde2622962
Nick Yasnohorodskyi <nikita.yasnohorodskyi@snyk.io>
2026-03-19 18:57:13 +0100

    fix: add missing Language using in HtmlSettingsScriptingBridgeTest [IDE-1701]

--- Snyk.VisualStudio.Extension.Tests/UI/Html/HtmlSettingsScriptingBridgeTest.cs
+++ Snyk.VisualStudio.Extension.Tests/UI/Html/HtmlSettingsScriptingBridgeTest.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.Sdk.TestFramework;
 using Moq;
 using Newtonsoft.Json;
 using Snyk.VisualStudio.Extension.Authentication;
+using Snyk.VisualStudio.Extension.Language;
 using Snyk.VisualStudio.Extension.Service;
 using Snyk.VisualStudio.Extension.Settings;
 using Snyk.VisualStudio.Extension.UI.Html;
