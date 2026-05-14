1e28da633de0e5b70116f12b61df0b70ea7d5884
Andrew Robinson Hodges <andrew.robinsonhodges@snyk.io>
2026-05-14 09:31:10 +0100

    feat: use webview2 for scan summary and issue details

--- Snyk.VisualStudio.Extension.2022/UI/Toolwindow/HtmlDescriptionPanel.xaml.cs
+++ Snyk.VisualStudio.Extension.2022/UI/Toolwindow/HtmlDescriptionPanel.xaml.cs
@@ -1,65 +1,118 @@
-﻿using System.Windows;
-using System.Windows.Navigation;
-using Snyk.VisualStudio.Extension.UI.Html;
+using System;
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json.Linq;
+using Serilog;
+using Snyk.VisualStudio.Extension.UI.Html;
 
 namespace Snyk.VisualStudio.Extension.UI.Toolwindow
 {
     public partial class HtmlDescriptionPanel : UserControl
     {
+        private static readonly ILogger Logger = LogManager.ForContext<HtmlDescriptionPanel>();
+
+        private readonly WebView2Host host;
         private IHtmlProvider htmlProvider;
-        private WebBrowserHostUIHandler _wbHandler;
 
         public HtmlDescriptionPanel()
         {
             this.InitializeComponent();
 
-            _wbHandler = new WebBrowserHostUIHandler(HtmlViewer)
-            {
-                IsWebBrowserContextMenuEnabled = false,
-                ScriptErrorsSuppressed = true,
-            };
+            var bridge = new SnykScriptManager(SnykVSPackage.ServiceProvider);
 
-            HtmlViewer.ObjectForScripting = new SnykScriptManager(SnykVSPackage.ServiceProvider);
-            _wbHandler.LoadCompleted += HtmlViewerOnLoadCompleted;
+            var dispatcher = new WebView2MessageDispatcher()
+                .Register("OpenFileInEditor", args => bridge.OpenFileInEditor(
+                    args[0].Value<string>(),
+                    args[1].Value<string>(),
+                    args[2].Value<string>(),
+                    args[3].Value<string>(),
+                    args[4].Value<string>()))
+                .Register("OpenLink", args => bridge.OpenLink(args[0].Value<string>()))
+                .Register("EnableDelta", args => bridge.EnableDelta(args[0].Value<bool>()))
+                .Register("GenerateFixes", args => bridge.GenerateFixes(args[0].Value<string>()))
+                .Register("ApplyFixDiff", args => bridge.ApplyFixDiff(args[0].Value<string>()))
+                .Register("SubmitIgnoreRequest", args => bridge.SubmitIgnoreRequest(
+                    args[0].Value<string>(),
+                    args[1].Value<string>(),
+                    args[2].Value<string>(),
+                    args[3].Value<string>()))
+                .Register("FocusToolWindow", _ => bridge.FocusToolWindow());
+
+            var userDataFolder = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Snyk", "WebView2", "description");
+
+            host = new WebView2Host(HtmlViewer, dispatcher, userDataFolder);
+
+            HtmlViewer.NavigationCompleted += OnNavigationCompleted;
+
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                try
+                {
+                    await host.InitializeAsync();
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "WebView2 initialization failed in description panel");
+                }
+            }).FireAndForget();
         }
 
-        private void HtmlViewerOnLoadCompleted(object sender, NavigationEventArgs e)
+        private async void OnNavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
         {
             try
             {
-                if (htmlProvider == null)
-                    return;
-                HtmlViewer.InvokeScript("eval", new string[] { htmlProvider.GetInitScript() });
+                if (htmlProvider == null) return;
+                await host.ExecuteScriptAsync(htmlProvider.GetInitScript());
             }
-            catch
+            catch (Exception ex)
             {
-
+                Logger.Error(ex, "Error running description init script");
             }
         }
 
         public void SetContent(string html, string product)
         {
-            if (string.IsNullOrEmpty(html))
-                return;
+            if (string.IsNullOrEmpty(html)) return;
             HtmlViewer.Visibility = Visibility.Visible;
 
             this.htmlProvider = HtmlProviderFactory.GetHtmlProvider(product);
-            if (this.htmlProvider == null)
-                return;
-            HtmlViewer.InvalidateVisual();
-            HtmlViewer.UpdateLayout();
-            html = htmlProvider.ReplaceCssVariables(html);
-            HtmlViewer.NavigateToString(html);
+            if (this.htmlProvider == null) return;
+
+            var themedHtml = htmlProvider.ReplaceCssVariables(html);
+
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                try
+                {
+                    await host.NavigateAsync(themedHtml);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Failed to navigate description panel");
+                }
+            }).FireAndForget();
         }
 
         public void Init()
         {
             HtmlViewer.Visibility = Visibility.Collapsed;
-            HtmlViewer.NavigateToString("<html><body style='margin:0;padding:0;'>Loading...</body></html>");
-            HtmlViewer.InvalidateVisual();
-            HtmlViewer.UpdateLayout();
+
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                try
+                {
+                    await host.NavigateAsync("<html><body style='margin:0;padding:0;'>Loading...</body></html>");
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Failed to initialize description panel content");
+                }
+            }).FireAndForget();
         }
     }
 }
-
--- Snyk.VisualStudio.Extension.2022/UI/Toolwindow/SummaryHtmlPanel.xaml.cs
+++ Snyk.VisualStudio.Extension.2022/UI/Toolwindow/SummaryHtmlPanel.xaml.cs
@@ -1,72 +1,116 @@
-﻿using System.Windows;
-using System.Windows.Navigation;
+using System;
+using System.IO;
+using System.Windows.Controls;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json.Linq;
+using Serilog;
 using Snyk.VisualStudio.Extension.UI.Html;
-using UserControl = System.Windows.Controls.UserControl;
 
 namespace Snyk.VisualStudio.Extension.UI.Toolwindow
 {
     public partial class SummaryHtmlPanel : UserControl
     {
+        private static readonly ILogger Logger = LogManager.ForContext<SummaryHtmlPanel>();
+
+        private readonly WebView2Host host;
         private IHtmlProvider htmlProvider;
-        private WebBrowserHostUIHandler _wbHandler;
 
         public SummaryHtmlPanel()
         {
             this.InitializeComponent();
 
-            _wbHandler = new WebBrowserHostUIHandler(SummaryHtmlViewer)
-            {
-                IsWebBrowserContextMenuEnabled = false,
-                ScriptErrorsSuppressed = true,
-            };
+            var bridge = new SnykScriptManager(SnykVSPackage.ServiceProvider);
 
-            SummaryHtmlViewer.ObjectForScripting = new SnykScriptManager(SnykVSPackage.ServiceProvider);
-            _wbHandler.LoadCompleted += HtmlViewerOnLoadCompleted;
+            var dispatcher = new WebView2MessageDispatcher()
+                .Register("OpenFileInEditor", args => bridge.OpenFileInEditor(
+                    args[0].Value<string>(),
+                    args[1].Value<string>(),
+                    args[2].Value<string>(),
+                    args[3].Value<string>(),
+                    args[4].Value<string>()))
+                .Register("OpenLink", args => bridge.OpenLink(args[0].Value<string>()))
+                .Register("EnableDelta", args => bridge.EnableDelta(args[0].Value<bool>()))
+                .Register("GenerateFixes", args => bridge.GenerateFixes(args[0].Value<string>()))
+                .Register("ApplyFixDiff", args => bridge.ApplyFixDiff(args[0].Value<string>()))
+                .Register("SubmitIgnoreRequest", args => bridge.SubmitIgnoreRequest(
+                    args[0].Value<string>(),
+                    args[1].Value<string>(),
+                    args[2].Value<string>(),
+                    args[3].Value<string>()))
+                .Register("FocusToolWindow", _ => bridge.FocusToolWindow());
+
+            var userDataFolder = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Snyk", "WebView2", "summary");
+
+            host = new WebView2Host(SummaryHtmlViewer, dispatcher, userDataFolder);
+
+            SummaryHtmlViewer.NavigationCompleted += OnNavigationCompleted;
+
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                try
+                {
+                    await host.InitializeAsync();
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "WebView2 initialization failed in summary panel");
+                }
+            }).FireAndForget();
         }
 
-        private void HtmlViewerOnLoadCompleted(object sender, NavigationEventArgs e)
+        private async void OnNavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
         {
             try
             {
-                if (htmlProvider == null)
-                    return;
-                SummaryHtmlViewer.InvokeScript("eval", new string[] { htmlProvider.GetInitScript() });
+                if (htmlProvider == null) return;
+                await host.ExecuteScriptAsync(htmlProvider.GetInitScript());
             }
-            catch
+            catch (Exception ex)
             {
-
+                Logger.Error(ex, "Error running summary init script");
             }
         }
+
         public void SetContent(string html, string product)
         {
-            if (string.IsNullOrEmpty(html))
-                return;
+            if (string.IsNullOrEmpty(html)) return;
 
             this.htmlProvider = HtmlProviderFactory.GetHtmlProvider("summary");
-            if (this.htmlProvider == null)
-                return;
-            html = htmlProvider.ReplaceCssVariables(html);
-            SummaryHtmlViewer.NavigateToString(html);
-            SummaryHtmlViewer.InvalidateVisual();
-            SummaryHtmlViewer.UpdateLayout();
+            if (this.htmlProvider == null) return;
+
+            var themedHtml = htmlProvider.ReplaceCssVariables(html);
+
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+            {
+                try
+                {
+                    await host.NavigateAsync(themedHtml);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Failed to navigate summary panel");
+                }
+            }).FireAndForget();
         }
 
         public void Init()
         {
-            ThreadHelper.JoinableTaskFactory.Run(async () =>
+            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
             {
-                var provider = (StaticHtmlProvider)HtmlProviderFactory.GetHtmlProvider("static");
-                var html = provider.ReplaceCssVariables(await provider.GetInitHtmlAsync());
-                
-                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                SummaryHtmlViewer.NavigateToString(html);
-
-                SummaryHtmlViewer.InvalidateVisual();
-                SummaryHtmlViewer.UpdateLayout();
-            });
-            
+                try
+                {
+                    var provider = (StaticHtmlProvider)HtmlProviderFactory.GetHtmlProvider("static");
+                    var html = provider.ReplaceCssVariables(await provider.GetInitHtmlAsync());
+                    await host.NavigateAsync(html);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error(ex, "Failed to initialize summary panel content");
+                }
+            }).FireAndForget();
         }
     }
 }
-
