9a197607fd9db49a319783ec3f0107fb5b6d1b27
Bastian Doetsch <bastian.doetsch@snyk.io>
2026-05-15 08:15:12 +0200

    fix(IDE-1653): revert protocol bump to 24, add V25Feature guard
    Reverts ProtocolVersion back to "24" so main stays functional with
    preview builds. All v25 behavior will be gated behind V25Feature.Enabled
    (currently false) and flipped in the final cleanup PR once all phases land.

--- Snyk.VisualStudio.Extension.Tests/Language/LsConstantsTest.cs
+++ Snyk.VisualStudio.Extension.Tests/Language/LsConstantsTest.cs
@@ -6,9 +6,9 @@ namespace Snyk.VisualStudio.Extension.Tests.Language
     public class LsConstantsTest
     {
         [Fact]
-        public void ProtocolVersion_ShouldBe25()
+        public void ProtocolVersion_ShouldBe24()
         {
-            Assert.Equal("25", LsConstants.ProtocolVersion);
+            Assert.Equal("24", LsConstants.ProtocolVersion);
         }
 
         [Fact]
